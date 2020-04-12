75079ed14b5aaa63cbb1149a24d8f3631299cf02
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2020-04-12 23:05:43 +0100

    Added config.php and tests for .env.example

--- config/config.php
+++ config/config.php
@@ -1,9 +1,22 @@
 <?php
+
 declare(strict_types=1);
 
 /*
- * You can place your custom package configuration in here.
+ * Configuration for Watson API. Any values in .env file will overwrite the defaults.
  */
 return [
+    'watsonApi' => [
+        'key' => env('WATSON_API_KEY'),
+        'path' => env('WATSON_API_PATH', './storage/watson-api'),
+        'url' => env('WATSON_API_URL', 'https://api.us-south.text-to-speech.watson.cloud.ibm.com'),
+        'name' => env('WATSON_API_NAME', 'en-US_MichaelVoice'),
+    ],
+    'debug_blacklist' => [
+        '_ENV' => ['WATSON_API_KEY'],
 
+        '_SERVER' => ['WATSON_API_KEY'],
+
+        '_POST' => ['watson-api-key', 'watson-apikey'],
+    ],
 ];
